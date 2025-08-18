5adef1fb36da4e74d2f10009b28c385e25f24972
Dean <deanlt.santos@gmail.com>
2025-08-18 20:32:18 +0800

    Add basic structure for admin dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import SideBar from "./pages/global/SideBar";
+import TopBar from "./pages/global/TopBar";
+
 function App() {
   return <>
-  <div>
-    <main></main>
+  <div className="app">
+    {/* Left Sidebar */}
+    <SideBar />
+    
+    {/* Main Dashboard Content */}
+    <main className="main-content">
+      
+      {/* Top bar*/}
+      <TopBar />
+
+    </main>
   </div>
   </>;
 }
