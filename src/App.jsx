33196ecb1b0064f1924869b431432ccd2ab8d579
Dean <deanlt.santos@gmail.com>
2025-08-22 12:58:51 +0800

    Add routing to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
+import { Routes, Route} from 'react-router-dom';
 import SideBar from "./pages/global/SideBar";
 import TopBar from "./pages/global/TopBar";
 
 function App() {
   return (
-    <div className="h-screen box-border flex bg-gray-800">
-      {/*Main app container*/}
-      <SideBar />
-      {/* Dashboard Left Nav-Sidebar */}
+    <Routes>
+      <Route
+        path="/*"
+        element={
+          <div className="relative h-screen box-border flex bg-gray-800">
+            {/*Main app container*/}
+            <SideBar />
+            {/* Dashboard Left Nav-Sidebar */}
 
-      {/* Main Dashboard Content */}
+            {/* Main Dashboard Content */}
 
-      <main className="main-content">
-        <TopBar />
-        {/* Dashboard Top-bar*/}
-      </main>
-    </div>
+            <main className="main-content">
+              <TopBar />
+              {/* Dashboard Top-bar*/}
+            </main>
+          </div>
+        }
+      />
+    </Routes>
   );
 }
 
