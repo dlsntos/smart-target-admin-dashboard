ec3b9265b1d484ed33b8497d5239dc42ab2af630
Dean <deanlt.santos@gmail.com>
2025-08-22 16:17:12 +0800

    Add container for topbar (still not sure if topbar is going to be used)

--- src/pages/global/TopBar.jsx
+++ src/pages/global/TopBar.jsx
@@ -1,7 +1,10 @@
-function TopBar () {
-    return(
-        <></>
-    );
+function TopBar() {
+  return (
+    <div className="h-20 ml-64 mb-1 px-3 py-2">
+      <div className="h-full w-full bg-gray-600">
+				
+			</div>
+    </div>
+  );
 }
-
 export default TopBar;
