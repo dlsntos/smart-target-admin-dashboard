b801050e5fb3d7361a36dd613710cd22e4f4edcb
Dean <deanlt.santos@gmail.com>
2025-08-22 11:47:57 +0800

    add rounded corners to sidebar nav buttons

--- src/pages/global/SideBar.jsx
+++ src/pages/global/SideBar.jsx
@@ -1,46 +1,46 @@
 import { NavLink } from "react-router-dom";
 function SideBar() {
   return (
-    <div className="fixed h-full w-64 bg-gray-700">
+    <div className="fixed h-full w-64 bg-gray-700 overflow-hidden shadow-outline">
       <nav>
-        <ul className="flex-1 py-3">
+        <ul className="flex-1 p-3">
           <li>
-            <NavLink className="block p-3 pl-6 mb-2 text-gray-300
+            <NavLink className="block p-3 pl-6 mb-2 rounded-lg text-gray-300
 																hover:bg-gray-800
 															  transition-all duration-300 ease-in-out">
 							Demographic Analytics
 						</NavLink>
           </li>
           <li>
-            <NavLink className="block p-3 pl-6 mb-2 text-gray-300
+            <NavLink className="block p-3 pl-6 mb-2 rounded-lg text-gray-300
 																hover:bg-gray-800
 															  transition-all duration-300 ease-in-out">
 							Consent Analytics
 						</NavLink>
           </li>
           <li>
-            <NavLink className="block p-3 pl-6 mb-2 text-gray-300
+            <NavLink className="block p-3 pl-6 mb-2 rounded-lg text-gray-300
 																hover:bg-gray-800
 															  transition-all duration-300 ease-in-out">
 							Performance
 						</NavLink>
           </li>
           <li>
-            <NavLink className="block p-3 pl-6 mb-2 text-gray-300 
+            <NavLink className="block p-3 pl-6 mb-2 rounded-lg text-gray-300 
 																hover:bg-gray-800
 															  transition-all duration-300 ease-in-out">
 							Sales
 						</NavLink>
           </li>
           <li>
-            <NavLink className="block p-3 pl-6 mb-2 text-gray-300
+            <NavLink className="block p-3 pl-6 mb-2 rounded-lg text-gray-300
 																hover:bg-gray-800
 															  transition-all duration-300 ease-in-out">
 							Feedback
 						</NavLink>
           </li>
           <li>
-            <NavLink className="block p-3 pl-6 mb-2 text-gray-300 
+            <NavLink className="block p-3 pl-6 mb-2 rounded-lg text-gray-300 
 																hover:bg-gray-800
 															  transition-all duration-300 ease-in-out">
 							Settings
