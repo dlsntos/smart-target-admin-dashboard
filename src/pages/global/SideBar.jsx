4260b1cd8d0448cb5b1fed26212f58bf8c0fb468
Dean <deanlt.santos@gmail.com>
2025-08-22 09:05:37 +0800

    Fix formatting using Prettier extension

--- src/pages/global/SideBar.jsx
+++ src/pages/global/SideBar.jsx
@@ -1,43 +1,31 @@
 import { NavLink } from "react-router";
-function SideBar () {
-    return(
-        <div className="fixed h-full w-64 bg-blue-400">
-            <nav>
-                <ul>
-                    <li>
-                        <NavLink>
-                            Demographic Analytics
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink>
-                            Consent Analytics
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink>
-                            Performance
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink>
-                            Sales
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink>
-                            Feedback
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink>
-                            Settings
-                        </NavLink>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-    );
+function SideBar() {
+  return (
+    <div className="fixed h-full w-64 bg-blue-400">
+      <nav>
+        <ul>
+          <li>
+            <NavLink>Demographic Analytics</NavLink>
+          </li>
+          <li>
+            <NavLink>Consent Analytics</NavLink>
+          </li>
+          <li>
+            <NavLink>Performance</NavLink>
+          </li>
+          <li>
+            <NavLink>Sales</NavLink>
+          </li>
+          <li>
+            <NavLink>Feedback</NavLink>
+          </li>
+          <li>
+            <NavLink>Settings</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
 
 export default SideBar;
