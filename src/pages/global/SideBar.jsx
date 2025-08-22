b19772fa95626f932d0f7e6bba117f23247c84bd
Dean <deanlt.santos@gmail.com>
2025-08-22 08:47:45 +0800

    add sidebar layout structure

--- src/pages/global/SideBar.jsx
+++ src/pages/global/SideBar.jsx
@@ -1,6 +1,15 @@
 function SideBar () {
     return(
-        <></>
+        <div>
+            <ul>
+                <li>Demographic Analytics</li>
+                <li>Consent Analytics</li>
+                <li>Performance</li>
+                <li>Sales</li>
+                <li>Feedbacks</li>
+                <li>Settings</li>
+            </ul>
+        </div>
     );
 }
 
