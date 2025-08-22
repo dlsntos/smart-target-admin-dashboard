b67985004a22e9a51c1ddd5eebc9179c80d044c6
Dean <deanlt.santos@gmail.com>
2025-08-22 08:54:19 +0800

    add navbar tag to SideBar

--- src/pages/global/SideBar.jsx
+++ src/pages/global/SideBar.jsx
@@ -1,14 +1,16 @@
 function SideBar () {
     return(
-        <div>
-            <ul>
-                <li>Demographic Analytics</li>
-                <li>Consent Analytics</li>
-                <li>Performance</li>
-                <li>Sales</li>
-                <li>Feedbacks</li>
-                <li>Settings</li>
-            </ul>
+        <div className="fixed h-full w-64 bg-blue-400">
+            <nav>
+                <ul>
+                    <li>Demographic Analytics</li>
+                    <li>Consent Analytics</li>
+                    <li>Performance</li>
+                    <li>Sales</li>
+                    <li>Feedbacks</li>
+                    <li>Settings</li>
+                </ul>
+            </nav>
         </div>
     );
 }
