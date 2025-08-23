d35b17c4ced7562d18c23b8c5bdf2258f26513c6
Dean <deanlt.santos@gmail.com>
2025-08-23 21:27:38 +0800

    Add login forms to Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,20 @@
+import React, {useState} from 'react';
 function Login() {
   return (
 		<div>
-			
+			<form>
+				<label>
+					<span>Email</span>
+				</label>
+				<input type="email"/>
+				<label>
+					<span>Password</span>
+				</label>
+				<input type="password"/>
+				<div>
+					<button type="submit">Login</button>
+				</div>
+			</form>
 		</div>
 	);
 }
