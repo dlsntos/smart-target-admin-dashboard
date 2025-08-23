5110bb021a384024278e8db3658fd41a7e4b309d
Dean <deanlt.santos@gmail.com>
2025-08-23 22:09:00 +0800

    add new div container for fixed sizing of forms

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -11,28 +11,32 @@ function Login() {
     navigate("/dashboard");
   };
   return (
-    <div className="h-screen w-screen flex justify-center items-start bg-red-300">
-      <form onSubmit={handleSubmit} className="flex flex-col w-1/4 mt-40 p-3">
-        <label>
-          <span>Email</span>
-        </label>
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <label>
-          <span>Password</span>
-        </label>
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <div className="flex justify-center">
-          <button type="submit">Login</button>
-        </div>
-      </form>
+    <div className="h-screen w-screen bg-blue-300">
+      <div className="h-full w-full flex justify-center">
+        <form  className="flex flex-col mt-20 h-[500px] w-[300px] bg-red-400" 
+							 onSubmit={handleSubmit}
+				>
+          <label>
+            <span>Email</span>
+          </label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <label>
+            <span>Password</span>
+          </label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <div className="flex justify-center">
+            <button type="submit">Login</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
