aff80b384445944a3959e19172e004811efd6bd4
Dean <deanlt.santos@gmail.com>
2025-08-23 21:30:40 +0800

    Setup useState for Email and Password inputs

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,24 @@
 import React, {useState} from 'react';
 function Login() {
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
   return (
 		<div>
 			<form>
 				<label>
 					<span>Email</span>
 				</label>
-				<input type="email"/>
+				<input 
+					type="email"
+					value={email}
+					onChange={(e) => setEmail(e.target.value)}/>
 				<label>
 					<span>Password</span>
 				</label>
-				<input type="password"/>
+				<input 
+					type="password"
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}/>
 				<div>
 					<button type="submit">Login</button>
 				</div>
