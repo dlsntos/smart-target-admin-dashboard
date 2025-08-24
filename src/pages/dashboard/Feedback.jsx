8ab7d1bc8399ba1071a09ef33bb864fb7d339358
Dean <deanlt.santos@gmail.com>
2025-08-24 18:28:50 +0800

    create array of sample data of feedbacks

--- src/pages/dashboard/Feedback.jsx
+++ src/pages/dashboard/Feedback.jsx
@@ -1,9 +1,28 @@
-function Feedback () {
-	return(
-		<div className="flex flex-col items-center h-[calc(100vh-5rem)] ml-64">
-			
-			
-		</div>
-	);
+function Feedback() {
+  const sampleFeedback = [
+    {
+      email: "notarealemail9@gmail.com",
+      message:
+        "The whole store was very accommodating. However the long sleeves that I am looking for is not here",
+    },
+    {
+      email: "dalengdalen@gmail.com",
+      message:
+        "Ang dali gamitin ng site, nakikita ko agad yung kulay at size na available. Sana mas madagdagan pa yung stock ng oversized shirts kasi mabilis maubos!",
+    },
+    {
+      email: "superflocious9@gmail.com",
+      message:
+        "Maganda yung site kasi hindi puro lalaki lang—napansin ko may pang-babae na rin sa display, kaya mas confident ako bumili.",
+    },
+    {
+      email: "realread9do@gmail.com",
+      message:
+        "Medyo nakalilito sa umpisa kung saan makikita yung cart, pero once nakita ko na, smooth na yung checkout.",
+    },
+  ];
+  return (
+    <div className="flex flex-col items-center h-[calc(100vh-5rem)] ml-64"></div>
+  );
 }
 export default Feedback;
