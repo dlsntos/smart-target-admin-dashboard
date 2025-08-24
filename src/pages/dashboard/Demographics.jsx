a50f9e610f729e3493e51f9d472df02aab21bcfd
Dean <deanlt.santos@gmail.com>
2025-08-24 17:46:45 +0800

    add h1 heading

--- src/pages/dashboard/Demographics.jsx
+++ src/pages/dashboard/Demographics.jsx
@@ -1,7 +1,8 @@
 import BarChartComponent from "../../components/BarChart";
 function Demographics() {
   return (
-    <div className="h-[calc(100vh-5rem)] ml-64 flex justify-center">
+    <div className="flex flex-col items-center h-[calc(100vh-5rem)] ml-64">
+      <h1 className="my-5 text-2xl text-gray-200 font-lupio">Demographic Analytics</h1>
 			<BarChartComponent />
     </div>
   );
