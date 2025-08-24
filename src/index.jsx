02033973761ffe4de7a40d01a1daa1d7825aa6b1
Dean <deanlt.santos@gmail.com>
2025-08-24 17:34:20 +0800

    add temporary style for now

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,9 @@
-function Dashboard (){
-    return(
-        <></>
-    );
+function Dashboard() {
+  return (
+    <div className="h-[calc(100vh-5rem)] ml-64 bg-red-500">
+
+    </div>
+  );
 }
 
 export default Dashboard;
