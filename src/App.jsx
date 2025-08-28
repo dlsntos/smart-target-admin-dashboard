328d71668b1967a124064b358d2c16cbd24b3a06
Dean <deanlt.santos@gmail.com>
2025-08-28 17:49:10 +0800

    removed unused routes and imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import SideBar from "./pages/global/SideBar";
-import TopBar from "./pages/global/TopBar";
 import Login from "./pages/Login";
 import Dashboard from ".";
-import AdPerformance from "./pages/dashboard/AdPerformance";
 import Demographics from "./pages/dashboard/Demographics";
 import ConsentAnalytics from "./pages/dashboard/ConsentAnalytics";
 import Feedback from "./pages/dashboard/Feedback";
+import Sales from "./pages/dashboard/Sales";
 
 function App() {
   return (
@@ -19,16 +18,14 @@ function App() {
           <div className="relative h-screen w-screen box-border flex bg-gray-800 overflow-hidden">
             {/* Dashboard Left Nav-Sidebar */}
             <SideBar />
-
             {/* Main Dashboard Content */}
             <main className="flex flex-col h-full w-full">
-              {/* Dashboard Top-bar*/}
               <div className="flex-1 overflow-auto">
                 <Routes>
                   <Route path="/" element={<Dashboard />} />
-                  <Route path="ad-performance" element={<AdPerformance />} />
                   <Route path="consent-analytics" element={<ConsentAnalytics />} />
                   <Route path="feedback" element={<Feedback />} />
+                  <Route path="sales" element={<Sales />} />
                   <Route path="demographic" element={<Demographics />} />
                 </Routes>
               </div>
