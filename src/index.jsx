f25d2a9fde986e565fadf2526921da3e3d5f6897
Dean <deanlt.santos@gmail.com>
2025-08-28 15:42:04 +0800

    revert cards to original size

--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,7 @@ function Dashboard() {
     <div className="h-full sm:ml-0 md:ml-64 bg-gray-800 p-8">
       <h1 className="text-3xl font-lupio text-gray-100 mb-8">Dashboard</h1>
       <div className="grid grid-rows-2 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        <div className="h-64 col-span-3 bg-gray-700 rounded-lg p-6 shadow">
+        <div className="h-64 bg-gray-700 rounded-lg p-6 shadow">
           <h2 className="text-xl text-gray-200 mb-2">Demographic Analytics</h2>
           <p className="text-gray-400">View age, gender, and other demographic stats.</p>
         </div>
