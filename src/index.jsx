f5cddb976f098d47fb094e3d5264f7e4b5278ef5
Dean <deanlt.santos@gmail.com>
2025-08-29 09:39:39 +0800

    adjust padding for medium screens

--- src/index.jsx
+++ src/index.jsx
@@ -3,9 +3,9 @@ import ConsentTrendChart from "./components/ConsentTrendOverTime";
 
 function Dashboard() {
   return (
-    <div className="h-full sm:ml-0 md:ml-64 bg-gray-800 p-8">
-      <h1 className="text-3xl font-lupio text-gray-100 mb-8">Dashboard</h1>
-      <div className="grid grid-rows-2 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+    <div className="h-full sm:ml-0 lg:ml-64 bg-gray-800 p-8">
+      <h1 className="text-3xl font-lupio text-gray-100 mb-8 text-center lg:text-left">Dashboard</h1>
+      <div className="grid grid-rows-2 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 pb-10 md:mb-0">
         <div className="h-64 bg-gray-700 rounded-lg p-6 shadow">
           <h2 className="text-xl text-gray-200 mb-2">Demographic Analytics</h2>
           <p className="text-gray-400">View age, gender, and other demographic stats.</p>
