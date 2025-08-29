420f7bae81b255f8e1f48bb3b3b4a32e654512ff
Dean <deanlt.santos@gmail.com>
2025-08-29 09:33:07 +0800

    tweak padding and margin for medium screens

--- src/pages/dashboard/ConsentAnalytics.jsx
+++ src/pages/dashboard/ConsentAnalytics.jsx
@@ -3,18 +3,18 @@ import PieChartComponent from "../../components/PieChart";
 import BackButton from "../../components/BackButton";
 function ConsentAnalytics() {
   return (
-    <div className="flex flex-col items-center h-full md:ml-64 p-5 overflow-y-auto">
+    <div className="flex flex-col items-center h-full lg:ml-64 p-5 overflow-y-auto">
       <div className="w-full flex justify-start mb-4">
   			<BackButton />
 			</div>
       <h1 className="my-5 text-2xl text-gray-200 font-lupio">
         Consent Analytics
       </h1>
-      <div className="flex flex-col md:flex-row gap-1 h-full w-full">
+      <div className="flex flex-col lg:flex-row gap-1 h-full w-full">
         <div className="flex-1 min-h-[250px] md:min-h-[300px] lg:min-h-[350px] flex items-center justify-center bg-gray-800 p-4 rounded-2xl shadow">
           <PieChartComponent />
         </div>
-        <div className="flex-1 min-h-[250px] md:min-h-[300px] lg:min-h-[350px] flex items-center justify-center bg-gray-800 p-4 rounded-2xl shadow">
+        <div className="flex-1 min-h-[250px] md:min-h-[300px] md:p-20 lg:min-h-[350px] flex items-center justify-center bg-gray-800 p-4 rounded-2xl shadow">
           <ConsentTrendChart />
         </div>
       </div>
