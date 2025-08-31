76c41c72a50b881d11c3b3aa0e0f6b8e496c9f60
Dean <deanlt.santos@gmail.com>
2025-08-31 16:21:36 +0800

    update api

--- src/api/api.js
+++ src/api/api.js
@@ -1,15 +1,16 @@
-import axios from 'axios';
+import axios from "axios";
 
 const API_BASE = "http://localhost:5000";
 
-export const createSale = (saleData) => {
-  return axios.post(`${API_BASE}/api/sales`, saleData);
+export const createSale = async (data) => {
+  const res = await axios.post(`${API_BASE}/api/sales`, data);
+  return res;
 };
 
 export const fetchFeedbackData = (feedbackData) => {
   return axios.get(`${API_BASE}/api/get-feedback`, feedbackData);
 };
 
-export const fetchSalesData = (salesData) => {
-  return axios.get(`${API_BASE}/api/get-sales`, salesData);
+export const fetchSalesData = () => {
+  return axios.get(`${API_BASE}/api/get-sales`);
 };
